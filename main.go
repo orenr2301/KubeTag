6036fd891597f77c6784bd38f0d2ee6f2267c831
OrdevZen <117763723+orenr2301@users.noreply.github.com>
2025-06-08 21:29:53 +0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	
+	"encoding/json"
+	"fmt"
+	"ns-manager/privileger/pkg/watcher"
+	"os"
+
+	
+	
+)
+
+
+func main() {
+
+	namespaceEnv := os.Getenv("NAMESPACES")
+	var namespaces []watcher.NamespaceConfig
+	if err := json.Unmarshal([]byte(namespaceEnv), &namespaces); err != nil {
+		panic(err.Error())
+	} 
+
+	
+
+	// Get Deafult Variables from envinronemnt vairables // need to handled the error in case there no mapping under that default vairables
+	defdefaultLabelsEnv := os.Getenv("DEFAULT_LABELS") 	
+	var defaultLabels map[string]string
+	if err := json.Unmarshal([]byte(defdefaultLabelsEnv), &defaultLabels); err != nil {
+		panic(err)
+	}
+		//Applying label to all namespaces
+
+
+	clientset, err := watcher.Connector()
+	if err != nil {
+		fmt.Errorf("Couldnt connect cluster or getting apiserver: %v", err)
+
+	}
+
+	//Iterating over each namesapce and apply labels
+	for _, ns := range namespaces {
+		mergedLabels := make(map[string]string)
+		if len(defaultLabels) > 0 {
+			for key, value := range defaultLabels {
+				mergedLabels[key] = value
+			}
+		}
+		for key, value := range ns.Labels {
+			mergedLabels[key] = value 
+		}
+
+		err = watcher.LabelNamespace(clientset, ns.Name, mergedLabels)
+		if err != nil {
+			fmt.Printf("Error labeling namesapce %s: %v\n", ns.Name, err)
+		} else {
+			fmt. Printf("Namespace %s labels successfuly.\n", ns.Name)
+		}
+	}
+
+	watcher.WatchNS(clientset, defaultLabels)
+	
+}
